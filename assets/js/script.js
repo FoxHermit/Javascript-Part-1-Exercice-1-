477e2aa7a503299669f7642fc8c76bd858e5304b
FoxHermit <62595041+FoxHermit@users.noreply.github.com>
2020-04-29 15:16:01 +0200

    Premier cours corrigé

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// Je crée une variable "firstname" et j'attribue comme valeur une chaîne de caractère (ici 'FoxHermit'), ah et pas besoin de fermer les slashs pour le commentaire, c'est ligne par ligne
+var firstname = 'FoxHermit';
+// Je demande à ce que ma variable "firstname" s'affiche dans uen boîte de dialoguqe à la fonction "alert()"
+alert(firstname);
